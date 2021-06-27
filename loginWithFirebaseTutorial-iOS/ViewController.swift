d0c9138f0ac3a53028af85256c78249b09dba0b8
hyungyu Kim <ypsophila97@icloud.com>
2021-06-27 23:46:49 +0900

    [chore] 프로젝트 설정 및 .gitignore, CocoaPods 설정
    ### Descriptions
    - 프로젝트 설정
    - .gitignore 파일 추가
    - CocoaPods pod install

---
+++ loginWithFirebaseTutorial-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loginWithFirebaseTutorial-iOS
+//
+//  Created by kimhyungyu on 2021/06/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
